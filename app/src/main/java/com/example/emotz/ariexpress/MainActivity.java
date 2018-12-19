7e0214af701ea2790b4baec9fbd8a2229e61a41e
AlexandraFishman <alexyafish@gmail.com>
2018-12-20 00:12:37 +0200

    Tried to add ListView with Firebase

--- app/src/main/java/com/example/emotz/ariexpress/MainActivity.java
+++ app/src/main/java/com/example/emotz/ariexpress/MainActivity.java
@@ -5,11 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button_register;
     private Button button_login;
+
+    private Firebase mRef;
+
+    private ArrayList<String> mUsername =  new ArrayList<>();
+
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +51,43 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(new Intent(getApplicationContext(),
                             LogInActivity.class));
                 }
+            }
+        });
+
+        mRef = new Firebase("https://ariexpress-3bb59.firebaseio.com/Product");
+
+        mListView = (ListView) findViewById(R.id.listView);
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, mUsername);
+
+        mListView.setAdapter(arrayAdapter);
+
+        mRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String value = dataSnapshot.getValue(String.class);
+                mUsername.add(value);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
             }
         });
     }
