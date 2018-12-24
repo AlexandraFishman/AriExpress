ef0f6c1728f9e13f4eb0be19a12830e60685b66f
Elad Motzny <emotzny@gmail.com>
2018-12-24 16:00:16 +0200

    Log in and registering is working. Need to work on prompts and general app design

---
+++ app/src/main/java/com/example/emotz/ariexpress/LogInActivity.java
@@ -0,0 +1,17 @@
+package com.example.emotz.ariexpress;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class LogInActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+
+    }
+}
--- app/src/main/java/com/example/emotz/ariexpress/MainActivity.java
+++ app/src/main/java/com/example/emotz/ariexpress/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.emotz.ariexpress;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button_register;
+    private Button button_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button_register = (Button)findViewById(R.id.moveRegButt);
+        button_login = (Button)findViewById(R.id.moveLogButt);
+        button_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v == button_register){
+                    startActivity(new Intent(getApplicationContext(),
+                            RegisterActivity.class));
+                }
+            }
+        });
+        button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v == button_login){
+                    startActivity(new Intent(getApplicationContext(),
+                            LogInActivity.class));
+                }
+            }
+        });
     }
 }
