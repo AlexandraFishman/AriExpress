eac7ab662ebd7d696aa8629416e03ffd61eda789
AlexandraFishman <alexyafish@gmail.com>
2018-12-26 23:47:57 +0200

    staring the creation of the remove button

--- app/src/main/java/com/example/emotz/ariexpress/AdminActivity.java
+++ app/src/main/java/com/example/emotz/ariexpress/AdminActivity.java
@@ -21,13 +21,13 @@ public class AdminActivity extends AppCompatActivity {
     private EditText productName;
     private EditText productPrice;
     private EditText productQuantity;
+    private EditText productId;
 
     private DatabaseReference productsDatabase;
 
     public void addNewItem(EditText productName, EditText productPrice, EditText productQuantity){
             Product newProduct = new Product(productName.getText().toString(), Double.parseDouble(productPrice.getText().toString()), Integer.parseInt(productQuantity.getText().toString()));
             productsDatabase = FirebaseDatabase.getInstance().getReference().child("Products").push();
-            productsDatabase.setValue(newProduct)
         productsDatabase.setValue(newProduct)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
@@ -54,12 +54,24 @@ public class AdminActivity extends AppCompatActivity {
         productName = (EditText)findViewById(R.id.ProductNameTextBox);
         productPrice = (EditText)findViewById(R.id.productPriceTextBox);
         productQuantity = (EditText)findViewById(R.id.productQuantityTextBox);
+        productId = (EditText)findViewById(R.id.productIdTextBox2);
 
         addProductButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (v == addProductButton){
                     addNewItem(productName,productPrice,productQuantity);
+                }
+            }
+        });
+
+        removeProductButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v == removeProductButton){
+//                    removeItem();
+                    Toast.makeText(AdminActivity.this,productId.getText().toString(),
+                            Toast.LENGTH_LONG).show();
                 }
             }
         });
